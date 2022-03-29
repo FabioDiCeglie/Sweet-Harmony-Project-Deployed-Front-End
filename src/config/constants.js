db38fca38dec748640da3c6e0227df66274804c2
Fabio Di Ceglie <93951206+FabioDiCeglie@users.noreply.github.com>
2022-03-29 13:31:17 +0200

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,2 @@
-export const apiUrl =
-  process.env.API_URL || "https://sweetharmony.herokuapp.com/";
+export const apiUrl ="https://sweetharmony.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
